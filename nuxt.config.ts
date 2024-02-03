b48745dbae761c4c10783ad5595e612b9aa827c6
Anzhelika99 <anzhelika.nitser@yandex.ru>
2024-02-03 11:08:42 +0700

    Create menu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,19 @@ export default defineNuxtConfig({
   modules: [
       '@nuxt/image',
       '@nuxtjs/tailwindcss',
-      'shadcn-nuxt'
+      'nuxt-icon',
+      'shadcn-nuxt',
+      [
+          '@nuxtjs/google-fonts',
+          {
+              families: {
+                  Lato: {
+                      wght: [300, 400, 700],
+                      ital: [300],
+                  },
+              },
+          },
+      ],
   ],
     shadcn: {
         prefix: 'Ui',
