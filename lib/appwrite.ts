c56767b9f63145f97360baf95be20e962c89afe7
Anzhelika99 <anzhelika.nitser@yandex.ru>
2024-02-17 13:45:56 +0700

    Change 4 spaces to 2 in TS files

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -4,8 +4,8 @@ import { Account, Client, Databases, Storage } from "appwrite";
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(APP_WRITE_ID);
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(APP_WRITE_ID);
 
 export const account = new Account(client)
 export { ID } from 'appwrite'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: [
-      '@nuxt/image',
-      '@nuxtjs/tailwindcss',
-      'nuxt-icon',
-      'shadcn-nuxt',
-      '@pinia/nuxt',
-      [
-          '@nuxtjs/google-fonts',
-          {
-              families: {
-                  Lato: {
-                      wght: [300, 400, 700],
-                      ital: [300],
-                  },
-              },
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
           },
-      ],
+        },
+      },
+    ],
   ],
     shadcn: {
-        prefix: 'Ui',
-        componentDir: './components/ui'
+      prefix: 'Ui',
+      componentDir: './components/ui'
     },
     pinia: {
-        storesDirs: ['./stores/**'],
+      storesDirs: ['./stores/**'],
     },
 })
